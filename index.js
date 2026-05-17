d1dbee514043b6a5052803e878b0733eba1d07ec
Renee Jun <rjun@terpmail.umd.edu>
2026-05-17 16:15:18 -0400

    message

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 const supabaseClient = require('@supabase/supabase-js');
+const { isValidStateAbbreviation } = require('usa-state-validator');
+const dotenv = require('dotenv');
 
 const app = express();
 const port = 3000;
@@ -14,11 +15,59 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
 
-// app.get("/test", async (req, res) => {
-//   const result = await supabase.from("customer").select("*")
+app.get('/', (req, res) => {
+  res.sendFile('public/Customers.html', { root: __dirname });
+});
 
-//   res.json(result);
-// });
+app.get('/customers', async (req, res) => {
+  console.log('Attempting to get all customers!');
+
+  const { data, error } = await supabase.from('customer').select();
+
+  if (error) {
+    console.log(`Error: ${error}`);
+    res.statusCode = 500;
+    res.send(error);
+  } else {
+    console.log('Recieved Data:', data.length);
+    res.json(data);
+  }
+});
+
+app.post('/customer', async (req, res) => {
+  console.log('Adding Customer');
+  console.log(`Request: ${JSON.stringify(req.body)}`);
+
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const state = req.body.state;
+
+  if (!isValidStateAbbreviation(state)) {
+    console.log(`State: ${state} is invalid`);
+    res.statusCode = 400;
+    res.json({
+      message: `${state} is not a valid 2 Letter Abbreviation for State`,
+    });
+    return;
+  }
+
+  const { data, error } = await supabase
+    .from('customer')
+    .insert({
+      customer_first_name: firstName,
+      customer_last_name: lastName,
+      customer_state: state,
+    })
+    .select();
+
+  if (error) {
+    console.log(`Error: ${error}`);
+    res.statusCode = 500;
+    res.send(error);
+  } else {
+    res.json(data);
+  }
+});
 
 app.listen(port, () => {
   console.log(`App is available on port: ${port}`);
